d213d2084f87ccb54103e74b79e5a89fec5017b8
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-04-08 22:26:04 +0300

    2.2. Рефакторинг - Площадь треугольника. [#122642 #286148]

--- src/test/java/ru/job4j/oop/TriangleTest.java
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -3,6 +3,7 @@ package ru.job4j.oop;
 import static org.hamcrest.Matchers.closeTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TriangleTest {
@@ -15,5 +16,15 @@ public class TriangleTest {
         Triangle triangle = new Triangle(a, b, c);
         double rsl = triangle.area();
         assertThat(rsl, closeTo(8, 0.001));
+    }
+
+    @Test
+    public void when12nd42and12ThenMinus1() {
+        Point a = new Point(1, 2);
+        Point b = new Point(4, 2);
+        Point c = new Point(1, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        double rsl = triangle.area();
+        assertThat(rsl, closeTo(-1, 0.001));
     }
 }
