f0572dd8324d6a7776867ccce711c56be30bafed
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-04-11 12:01:46 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,28 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] arr = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                arr[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(arr, count);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] byName = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                byName[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(byName, count);
     }
 
     public Item findById(int id) {
