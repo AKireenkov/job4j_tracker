13967e7f9d52fbad570850ec79498f3a797cf2b9
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-04-13 12:26:27 +0300

    3. Интерфейс Input [#181092 #288155]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,17 @@ package ru.job4j.poly;
 public class Bus implements Transport {
     @Override
     public void drive() {
-
+        System.out.println("Автобус двигается по заранее намечнному маршруту");
     }
 
     @Override
     public void passengers(int amount) {
-
+        System.out.println("Количество пассажиров в автобусе: " + amount);
     }
 
     @Override
     public int refuel(int amount) {
-        return 0;
+        int costPerLiter = 10;
+        return amount * costPerLiter;
     }
 }
