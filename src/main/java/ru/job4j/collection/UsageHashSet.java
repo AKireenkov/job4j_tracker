cd4608affade3cfeeed29f5da5cbdc06663574a2
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-04-22 08:52:52 +0300

    0. Set, HashSet, Iterator. [#10095]

---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,22 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<>();
+        autos.add("Lada");
+        autos.add("BMW");
+        autos.add("Volvo");
+        autos.add("Toyota");
+
+        for (String car : autos) {
+            System.out.println(car);
+        }
+    }
+}
+
+/*
+ Добавьте в коллекцию autos марки машин: Lada, BMW, Volvo, Toyota.
+3. Добавьте цикл for-each и выведите элементы коллекции HashSet на консоль.
+ */
