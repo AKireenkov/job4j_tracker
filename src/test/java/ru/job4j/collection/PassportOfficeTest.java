32042437692454365ecbfc6477fb8f5202d927cb
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-04-25 16:13:19 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void addWithDuplicate() {
+        Citizen one = new Citizen("2f44a", "Petr Arsentev");
+        Citizen two = new Citizen("2f44a", "Andrey Kireenkov");
+        PassportOffice office = new PassportOffice();
+        office.add(one);
+        office.add(two);
+        assertThat(office.get(one.getPassport()), is(one));
+    }
+}
