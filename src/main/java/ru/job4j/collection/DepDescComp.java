35015626a6019b4ef614b4c6a297f2de73fbed35
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-01 08:52:41 +0300

    2. Отсортировать департаменты [#34352 #295716]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -5,14 +5,9 @@ import java.util.Comparator;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        if (Character.compare(o1.charAt(0), o2.charAt(0)) == 0 && Character.compare(o1.charAt(1), o2.charAt(1)) == 0) {
-            for (int i = 1; i < Math.min(o1.length(), o2.length()); i++) {
-                if (Character.compare(o1.charAt(o1.length() - i), o2.charAt(o2.length() - i)) != 0) {
-                    break;
-                }
-            }
-            return o1.compareTo(o2);
-        }
-        return o2.compareTo(o1);
+        String[] str1 = o1.split("/");
+        String[] str2 = o2.split("/");
+        int rsl = str2[0].compareTo(str1[0]);
+        return rsl == 0 ? o1.compareTo(o2) : rsl;
     }
 }
