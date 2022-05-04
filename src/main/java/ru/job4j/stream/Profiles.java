8e152cb48e4687f7dded7410c6de85b9f4dde8da
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-04 14:05:36 +0300

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,9 +6,18 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
-
         return profiles
                 .stream()
-                .map(profile -> profile.getAddress()).collect(Collectors.toList());
+                .map(Profile::getAddress)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
+        return profiles
+                .stream()
+                .map(Profile::getAddress)
+                .sorted()
+                .distinct()
+                .collect(Collectors.toList());
     }
 }
