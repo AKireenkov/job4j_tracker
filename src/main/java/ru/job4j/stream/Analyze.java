ee4a8b2f163e365058eed5191494c953a65239e9
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-05-08 15:49:40 +0300

    2. Аттестация [#49777 #298563]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -49,7 +49,7 @@ public class Analyze {
         return stream.flatMap(s -> s.getSubjects().stream())
                 .collect(
                         Collectors.groupingBy(
-                                Subject::getName, LinkedHashMap::new, Collectors.summingDouble(Subject::getScore)
+                                Subject::getName, Collectors.summingDouble(Subject::getScore)
                         ))
                 .entrySet()
                 .stream()
