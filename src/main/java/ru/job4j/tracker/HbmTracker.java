7f318e0409b055ffa288256bbf6ea4fcb1fe4113
andreykireenkov <kireeshka32@gmail.com>
2023-03-15 21:07:47 +0300

    3. HbmTracker [#287705]

---
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -0,0 +1,101 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+
+    @Override
+    public void close() {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+
+    @Override
+    public Item add(Item item) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return item;
+    }
+
+    @Override
+    public boolean replace(int id, Item item) {
+        Session session = sf.openSession();
+        boolean rsl = false;
+        try {
+            session.beginTransaction();
+            session.update(item);
+            session.getTransaction().commit();
+            rsl = true;
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return rsl;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        Session session = sf.openSession();
+        var affectRows = 0;
+        try {
+            session.beginTransaction();
+            affectRows = session.createQuery("DELETE Item WHERE id = :fId")
+                    .setParameter("fId", id)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception ex) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+        return affectRows > 0;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<Item> items = session.createQuery("from Item", Item.class).list();
+        session.getTransaction().commit();
+        session.close();
+        return items;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        List<Item> items = session.createQuery("from Item where name=:key", Item.class)
+                .setParameter("key", key)
+                .list();
+        session.getTransaction().commit();
+        session.close();
+        return items;
+    }
+
+    @Override
+    public Item findById(int id) {
+        Session session = sf.openSession();
+        session.beginTransaction();
+        var item = session.get(Item.class, id);
+        session.getTransaction().commit();
+        session.close();
+        return item;
+    }
+}
